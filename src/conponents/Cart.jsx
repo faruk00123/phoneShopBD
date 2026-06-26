50fe05433997bd3e7ac9a1893508b5766256a38f
Md.Omar <omar013faruka@gmail.com>
2026-06-27 00:07:08 +0600

    change

--- src/conponents/Cart.jsx
+++ src/conponents/Cart.jsx
@@ -4,6 +4,10 @@ import { FaPlus } from 'react-icons/fa'
 const cart = ({items, CartClick}) => {
   return (
      <div className='bg-white my-2 mx-1 rounded shadow-2xl relative'>
+        {
+         items.url && <img src={items.url} alt='No Photo'/>
+        }
+        
         <h1 className='text-center mt-2 text-xl font-semibold'>{items.name}</h1>
         <p className='mt-2 pl-1'>Detels: <span className='text-sm text-gray-500'>{items.dec}</span></p>
         <h3 className='text-xl pl-1 font-semibold mb-10 mt-2'>Price: <span className='font-bold'>{items.pri}</span></h3>
